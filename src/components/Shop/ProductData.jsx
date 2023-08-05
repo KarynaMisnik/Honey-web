3572c307d977575009dd95bb5050ef317933161d
KarynaMisnik <karinamisnik94@gmail.com>
2023-08-05 22:41:03 +0300

    new imgs added
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

---
+++ src/components/Shop/ProductData.jsx
@@ -0,0 +1,104 @@
+
+export const storeProducts =[
+    {
+        id: 1,
+        title: "",
+        img: "",
+        price: "",
+        info: "",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+        {
+        id: 1,
+        title: "",
+        img: "",
+        price: "",
+        info: "",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+        {
+        id: 1,
+        title: "",
+        img: "",
+        price: "",
+        info: "",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+        {
+        id: 1,
+        title: "",
+        img: "",
+        price: "",
+        info: "",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+        {
+        id: 1,
+        title: "",
+        img: "",
+        price: "",
+        info: "",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+        {
+        id: 1,
+        title: "",
+        img: "",
+        price: "",
+        info: "",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+        {
+        id: 1,
+        title: "",
+        img: "",
+        price: "",
+        info: "",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+        {
+        id: 1,
+        title: "",
+        img: "",
+        price: "",
+        info: "",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+        {
+        id: 1,
+        title: "",
+        img: "",
+        price: "",
+        info: "",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+        {
+        id: 1,
+        title: "",
+        img: "",
+        price: "",
+        info: "",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+]
