b08fe69548bc412d7cddb543b0181e0fcac2b17c
KarynaMisnik <karinamisnik94@gmail.com>
2023-08-08 20:20:51 +0300

    shop is modified
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

--- src/components/Shop/ProductList.jsx
+++ src/components/Shop/ProductList.jsx
@@ -8,7 +8,7 @@
 const CustomButton = styled(Button)(({ theme }) => ({
 textAlign: 'center',
 fontFamily: 'var(--main-font)',
-fontSize: '24px',
+fontSize: '18px',
 fontWeight: '700',
 background: 'var(--dark-brown)',
 color: 'var(--white)',
@@ -75,19 +75,24 @@ justifyContent:"center",
     padding: 2,
       }}>
 
-        <Grid container spacing={2}>
+        <Grid container spacing={2} sx={{padding: '1rem'}}>
 
        {data.map((values) => {
        return(
           <Grid item xs={12} sm={6} md={4} key={values.id} sx={{textAlign: 'center'}}>
-            <Paper elevation={3}>
-              <img src={values.src} alt={values.title}  sx={{width: '100%',
-    height: 'auto',}}/>
-              <h3>{values.title}</h3>
-              <p>{values.price}</p>
-              <Button variant="contained" color="primary">
-                More
-              </Button>
+            <Paper elevation={6}  style={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', borderRadius: '8px' }}>
+              <img src={values.src} alt={values.title} loading='lazy' style={{width: '100%', height: '500px', borderTopLeftRadius: '8px', borderTopRightRadius: '8px'}}/>
+              <div style={{display:'flex', justifyContent: 'center', alignItems:'center', padding: '0.5rem'}}>
+              <p className='product_list title'>{values.title}</p>
+              <p className='product_list amount'>{values.amount}</p>
+              <p className='product_list price'>{values.price}&#x20AC;</p>
+              <div style={{display:'flex', flexDirection: 'column', justifyContent: 'center', alignItems:'center', padding: '0.5rem'}}>
+              <CustomButton >
+            Buy 
+              </CustomButton>
+               <a className='product_list details'>Details</a>
+              </div>
+              </div>
             </Paper>
           </Grid>
         
