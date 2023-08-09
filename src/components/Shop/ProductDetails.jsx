b9fa7a7dff0632c4607002fc4199fd38aacb224e
KarynaMisnik <karinamisnik94@gmail.com>
2023-08-09 18:12:02 +0300

    json ProductDetails are modified
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

--- src/components/Shop/ProductDetails.jsx
+++ src/components/Shop/ProductDetails.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import productData from './productData.json'; // Import your JSON data
+import Grid from '@mui/material/Grid';
+
 
 function ProductDetails() {
   const {valuesId } = useParams();
@@ -11,14 +13,23 @@ function ProductDetails() {
   }
 
   return (
-    <div>
-      <img src={product.src} alt={product.alt}  style={{width:"500px", height:"500px" }} loading='lazy'/>
-      <h2>{product.title}</h2>
-      <p>{product.info}</p>
-      <p>Price: {product.price}</p>
-      <p>Amount: {product.amount}</p>
-      {/* Display other product details */}
-    </div>
+<>
+     <Grid container spacing={2}>
+    <Grid item xs={12} sm={6} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <img src={product.src} alt={product.alt} loading='lazy' style={{ width: '100%', height: 'auto', borderRadius: '10px', margin:'1.5rem' }} />
+      </Grid>
+      
+      <Grid item xs={12} sm={6}>
+        <div className='product_details_container'>
+          <p className='product_details title'>{product.title}</p>
+          <p className='product_details info'>{product.info}</p>
+          <p className='product_details price'><span>Price:</span> {product.price}&#x20AC;</p>
+          <p className='product_details amount'><span>Amount:</span> {product.amount}</p>
+        </div>
+      </Grid>
+    </Grid>
+
+</>
   );
 }
 
