93ba35f0776d4f3e90ade0349be2fc63d06cae30
KarynaMisnik <karinamisnik94@gmail.com>
2023-08-19 14:20:42 +0300

    changing in color palette layout
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () =>{
     <div className='bg-text'>
      <p> HONEY & <br/>HONEY PRODUCTS
 </p>
-    <Divider sx={{margin:'0 1rem', maxWidth:'100%', height: '3px',  backgroundColor: 'var(--light-gray)' }}></Divider>
+ <Divider sx={{ maxWidth:'100%', height: '7px',  backgroundColor: 'var(--rich-gray)', borderRadius: '50%' }}></Divider> 
 
 <p className="hero-text">Get fresh honey and organic honey products with just a one click</p>
     </div>
