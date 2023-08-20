a9f041b34b2dd7e62cb289aec857c451784423a5
KarynaMisnik <karinamisnik94@gmail.com>
2023-08-21 00:04:16 +0300

    AboutSection is modified
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

--- src/components/About/AboutSection.jsx
+++ src/components/About/AboutSection.jsx
@@ -7,19 +7,22 @@ export default function AboutSection() {
   return (
    <Grid container direction="column" spacing={2} sx={{backgroundImage: 'linear-gradient(to bottom, #fbe386, #fce58c, #fde792, #fde897, #feea9d, #ffe7aa, #ffe6b8, #ffe5c5, #ffe7dc, #ffeef2, #fff7fe, #ffffff)'}}>
       <Grid item>
-        <div  style={{ display:'flex', justifyContent:'center',alignItems:'center', background:'red' }}>
-          <img src={Beekeepers} style={{  boxShadow: '-15px -18px 0px -3px #000000', borderRadius:'10px'}}/>
-          <div>
-<p style={{fontFamily:'var(--abrilFatface-font)', fontSize:'3rem', marginBottom:'1.5rem'}}>Welcome to the sweet world of the Doe Family Honey!</p>
-<p style={{fontFamily:'var(--main-font)', fontSize:'2rem', marginBottom:'1.5rem'}}>We're a close-knit family passionate about sharing the natural goodness of our handcrafted honey products. From our family to yours, we invite you to savor the sweetness of nature's finest gift – pure, golden honey.</p>
-                  <Button sx={{padding:'2rem', background: 'var(--dark-yellow)', color:'var(--black)', fontFamily:'var(--abrilFatface-font)', fontSize:'1.5rem'}}>Learn More About Us</Button>
+        <div  className='imgText-wrapper' style={{ display:'flex', alignItems: 'center'}}>
+          <div className='img-container' style={{flex:'1', padding:'1rem ', padding:'2rem'}}>
+          <img src={Beekeepers} style={{  boxShadow: '-35px -35px 0px -3px #000000', borderRadius:'20px',  maxWidth: '100%', height: 'auto'}}/>
+          </div>
+
+          <div className='welcoming-text' style={{flex:'1', display:'flex', flexDirection:'column', textAlign:'center', padding:'2rem'}}>
+<p style={{fontFamily:'var(--abrilFatface-font)', fontSize:'3rem', marginBottom:'2rem'}}>Welcome to the sweet world of the Doe Family Honey!</p>
+<p style={{fontFamily:'var(--main-font)', fontSize:'2rem', marginBottom:'1.5rem', textAlign:'justify', marginBottom:'5rem'}}>We're a close-knit family passionate about sharing the natural goodness of our handcrafted honey products. From our family to yours, we invite you to savor the sweetness of nature's finest gift – pure, golden honey.</p>
+                  <Button sx={{padding:'2rem', background: 'var(--dark-yellow)', color:'var(--black)', fontFamily:'var(--abrilFatface-font)', fontSize:'1.5rem', textTransform:'none', '&:hover': {background: 'var(--dark-yellow)'}}}>Learn More About Us</Button>
 
           </div>
         </div>
       </Grid>
       <Grid item sx={{display:'flex', alignItems:'center', justifyContent:'center'}}>
         <div  style={{ padding: '20px', background:'var(--black)', width:'95%'}}>
-          <p style={{color: 'var(--yellow)', textAlign:'center', fontFamily:'var(--abrilFatface-font)', fontSize:'3rem'}}>Bees: Nature's tiny heroes, vital for pollination, essential for our food.</p>
+          <p style={{color: 'var(--yellow)', textAlign:'center', fontFamily:'var(--abrilFatface-font)', fontSize:'2.8rem'}}>Bees: Nature's tiny heroes, vital for pollination, essential for our food.</p>
         </div>
       </Grid>
       <Grid item>
@@ -28,8 +31,9 @@ export default function AboutSection() {
         </div>
       </Grid>
       <Grid item>
-        <div style={{ padding: '20px' }}>
-          Element 4
+        <div  style={{ display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', textAlign:'center' }}>
+          <p style={{fontFamily:'var(--abrilFatface-font)' , fontSize:'2rem'}}>Explore our selection of honey and honey-based delights</p>
+          <h1 style={{fontFamily:'var(--abrilFatface-font)', fontSize:'5rem', paddingBottom:'3rem'}}>GO SHOPPING</h1>
         </div>
       </Grid>
     </Grid>
