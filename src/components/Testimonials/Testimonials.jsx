69f34828c3db2892334011147cdfbe343fc1e14e
KarynaMisnik <karinamisnik94@gmail.com>
2023-09-02 23:30:06 +0300

    .
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -24,7 +24,7 @@ export default function Testimonials(){
             component="img"
             alt="Image"
             height="140"
-            image={Item} // Replace with the actual image URL
+            image={Item} 
           />
         </Grid>
         <Grid item xs={12} sm={8}>
