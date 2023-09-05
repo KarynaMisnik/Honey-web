00d4085da19c88bf5ec412f92a33255a3cbbf3af
KarynaMisnik <karinamisnik94@gmail.com>
2023-09-05 22:10:17 +0300

    .
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -5,7 +5,7 @@ import BeeError from '../../assets/img/bee-error.png';
 export default function Error(){
   return (
    <div className='error-container' style={{display:'flex',justifyContent:'center',alignItems:'center', background: "var(--light-yellow)", maxWidth: "100%", height: "auto", position: 'relative'}}>
-    <img className='workingBee' src={BeeError} alt="A cute bee with a wrench"></img>
+   <h1>Here must be smth</h1>
     <div className='text-container'>
 <p><span>404</span><br/> Oh, bzzz. <br></br>You are totally lost.</p>
     </div>
