a184725fe54be2fda4e7b59b20f28a08d33eef90
KarynaMisnik <karinamisnik94@gmail.com>
2023-09-10 22:48:01 +0300

    .
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -25,12 +25,18 @@ const shuffledProductData = shuffleArray(productData);
 export default function Testimonials(){
     return(
         <>
-       <h1 style={{textAlign:'center', padding:'1rem 0', fontFamily:'var(--main-font)', fontWeight:'bolder'}}>What our customers say</h1>
-  {shuffledProductData.slice(0, 3).map((cardItem) => (
-        <Grid container spacing={2} sx={{background:'green'}}>
-        <Grid item key={cardItem.id} xs={12} sm={4}>
+       <h1 style={{textAlign:'center', padding:'1rem 0', fontFamily:'var(--main-font)', fontWeight:'bolder'}}>
+        What our customers say</h1>
 
-          <CardContent sx={{ width:'300px', display: 'flex', flexDirection: 'column'}}>
+   <Box sx={{ background:'green', flexGrow:1}}>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12 }}>
+ 
+  {shuffledProductData.slice(0, 3).map((cardItem) => (
+        
+          
+        <Grid item key={cardItem.id} xs={2} sm={4} md={4} sx={{display:'flex', justifyContent:'center', background:'pink', width:'300px'}} >
+
+          <CardContent sx={{ width:'300px',  display: 'flex', flexDirection: 'column', margin:'0'}}>
             <img src={cardItem.reviewerImg} style={{borderRadius:'50%', border:'2px solid var(--dark-yellow)', width: '150px', height:'150px'}}></img>
             <Box display="flex" alignItems="center" mt={1}>
               
@@ -45,20 +51,17 @@ export default function Testimonials(){
 
             <Typography  color="var(--black)">{cardItem.review}</Typography>
             <Box display="flex" alignItems="center" mt={1}>
-              <Typography variant="body2" color="textSecondary">
+              <Typography  color="var(--black)">
                 {cardItem.dateReview}
               </Typography>
             </Box>
-            
-          
           </CardContent>
-        
-     
-    
     </Grid>
-     </Grid>
-    
+   
 ))}
+
+   </Grid>
+    </Box>
         </>
     )
 }
