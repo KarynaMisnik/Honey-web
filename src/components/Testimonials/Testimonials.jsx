85e27d7932ff87fd27a679365a841083f9c568a6
KarynaMisnik <karinamisnik94@gmail.com>
2023-09-13 21:44:04 +0300

    .
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,14 +1,6 @@
 import * as React from 'react';
-import Item from '../../assets/img/item1.png';
 import productData from '../Shop/productData.json'; 
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
 import Rating from '@mui/material/Rating';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
 
 
 function shuffleArray(array) {
@@ -28,13 +20,14 @@ export default function Testimonials(){
         <>
        <h1 style={{textAlign:'center', padding:'1rem 0', fontFamily:'var(--main-font)', fontWeight:'bolder'}}>
         What our customers say</h1>
-<div className='card-container'  style={{display:'flex', flexDirection: 'row', justifyContent:'space-around', alignItems:'center', }}>
+<div className='card-container'  style={{display:'flex', flexDirection: 'row', justifyContent:'space-around', alignItems:'center', padding:'4em 1.5em'}}>
 
 {shuffledProductData.slice(0, 3).map((cardItem) => (
 
 
-<div key={cardItem.id} className='card-content'  style={{display:'flex', flexDirection:'column', width:'400px', height:'500px', padding: '1.5em', background:'green'}} >
+<div key={cardItem.id} className='card-content'  style={{display:'flex', flexDirection:'column', width:'400px', height:'500px', padding: '1.5em', borderRadius:'15px', background:'var(--white)', boxShadow: '-22px -22px 0px 0px var(--black)'}} >
    <img src={cardItem.reviewerImg} style={{borderRadius:'50%', border:'2px solid var(--dark-yellow)', width: '150px', height:'150px'}}></img>
+<h2>{cardItem.reviewer}</h2>
  <Rating name="read-only" value={cardItem.value} readOnly />
 <h3>{cardItem.title}</h3>
 <p>{cardItem.review}</p>
