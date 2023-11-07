e6476fa7dc45b996c386e2892b52fafbdd0d9ae1
KarynaMisnik <karinamisnik94@gmail.com>
2023-11-07 17:03:54 +0200

    testimonials css added, testimonials component upd
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,94 +1,113 @@
-import * as React from 'react';
-import Divider from '@mui/material/Divider';
-import Typography from '@mui/material/Typography';
-import { Button, Grid, TextField } from '@mui/material';
-import Box from '@mui/material/Box';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import PhoneIcon from '@mui/icons-material/Phone';
-import EmailIcon from '@mui/icons-material/Email';
-import WatchLaterIcon from '@mui/icons-material/WatchLater';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import { NavLink } from 'react-router-dom';
-import { styled } from '@mui/material/styles';
+import * as React from "react";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+import { Button, Grid, TextField } from "@mui/material";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import PhoneIcon from "@mui/icons-material/Phone";
+import EmailIcon from "@mui/icons-material/Email";
+import WatchLaterIcon from "@mui/icons-material/WatchLater";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import { NavLink } from "react-router-dom";
+import { styled } from "@mui/material/styles";
 
 /* Text */
 const StyledTypography = styled(Typography)(({ theme }) => ({
-  color: 'var(--white)',
-  marginBottom: '8px',
-  fontFamily: 'var(--main-font)',
+  color: "var(--white)",
+  marginBottom: "8px",
+  fontFamily: "var(--main-font)",
   fontWeight: 700,
-  letterSpacing: '1px',
+  letterSpacing: "1px",
 }));
 
 /* Underline */
 const StyledDivider = styled(Divider)(({ theme }) => ({
-height:'2px', 
-width: '50px',
- backgroundColor: 'yellow', 
- margin:'6px 0 6px 0'
+  height: "2px",
+  width: "50px",
+  backgroundColor: "yellow",
+  margin: "6px 0 6px 0",
 }));
 
 /* Array of social media icons */
 const icons = [
-  <a href="https://twitter.com"><TwitterIcon sx={{color: 'var(--yellow)', fontSize: { xs: '2rem', lg: '2.5rem' }}}/></a>,
-  <a href="https://youtube.com"><YouTubeIcon sx={{color: 'var(--yellow)', fontSize: { xs: '2rem', lg: '2.5rem' }  }}/></a>,
-  <a href="https://linkedin.com"><LinkedInIcon sx={{color: 'var(--yellow)', fontSize: { xs: '2rem', lg: '2.5rem' }  }}/></a>,
-  <a href="https://instagram.com"><InstagramIcon sx={{color: 'var(--yellow)', fontSize: { xs: '2rem', lg: '2.5rem' }  }}/></a>,
-  <a href="https://facebook.com"><FacebookIcon sx={{color: 'var(--yellow)', fontSize: { xs: '2rem', lg: '2.5rem' }  }}/></a>
+  <a href="https://twitter.com">
+    <TwitterIcon
+      sx={{ color: "var(--yellow)", fontSize: { xs: "2rem", lg: "2.5rem" } }}
+    />
+  </a>,
+  <a href="https://youtube.com">
+    <YouTubeIcon
+      sx={{ color: "var(--yellow)", fontSize: { xs: "2rem", lg: "2.5rem" } }}
+    />
+  </a>,
+  <a href="https://linkedin.com">
+    <LinkedInIcon
+      sx={{ color: "var(--yellow)", fontSize: { xs: "2rem", lg: "2.5rem" } }}
+    />
+  </a>,
+  <a href="https://instagram.com">
+    <InstagramIcon
+      sx={{ color: "var(--yellow)", fontSize: { xs: "2rem", lg: "2.5rem" } }}
+    />
+  </a>,
+  <a href="https://facebook.com">
+    <FacebookIcon
+      sx={{ color: "var(--yellow)", fontSize: { xs: "2rem", lg: "2.5rem" } }}
+    />
+  </a>,
 ];
 
 /* Array of contact icons */
 const contactInfo = [
   {
-    icon: <LocationOnIcon/>,
-    text: '1 Tie, Lehtola Finland 00000',
+    icon: <LocationOnIcon />,
+    text: "1 Tie, Lehtola Finland 00000",
   },
   {
-    icon: <PhoneIcon/>,
-    text: '+358-123-456-789',
+    icon: <PhoneIcon />,
+    text: "+358-123-456-789",
   },
   {
-    icon: <EmailIcon/>,
-    text: 'beehoney@email.com',
+    icon: <EmailIcon />,
+    text: "beehoney@email.com",
   },
   {
-    icon: <WatchLaterIcon/>,
-    text: 'Mon - Fri, 9-AM - 6PM',
+    icon: <WatchLaterIcon />,
+    text: "Mon - Fri, 9-AM - 6PM",
   },
 ];
 
 const listItemTextStyle = {
-  fontWeight: '700',
-  fontFamily: 'var(--main-font)',
+  fontWeight: "700",
+  fontFamily: "var(--main-font)",
 };
 
 /* Submit button */
 const StyledButton = styled(Button)(({ theme }) => ({
-  backgroundColor: 'var(--yellow)',
-  borderRadius: '5px',
-  color: 'var(--black)',
+  backgroundColor: "var(--yellow)",
+  borderRadius: "5px",
+  color: "var(--black)",
   fontWeight: 900,
-  ':hover': {
-    backgroundColor: 'var(--yellow)',
+  ":hover": {
+    backgroundColor: "var(--yellow)",
   },
 }));
 
 /* Message form */
 const CustomTextField = styled(TextField)(({ theme }) => ({
-    width: '100%',
-    backgroundColor: 'var(--light-yellow)',
-    borderRadius: '5px',
+  width: "100%",
+  backgroundColor: "var(--light-yellow)",
+  borderRadius: "5px",
 }));
-
 
 function Item(props) {
   const { sx, ...other } = props;
@@ -97,9 +116,9 @@ function Item(props) {
       sx={{
         p: 1,
         m: 1,
-       
-        fontSize: '0.875rem',
-        fontWeight: '700',
+
+        fontSize: "0.875rem",
+        fontWeight: "700",
         ...sx,
       }}
       {...other}
@@ -107,129 +126,188 @@ function Item(props) {
   );
 }
 
-
 export default function Contact() {
   return (
- <div style={{ maxWidth: '100%', background: 'var( --black)', color:'var(--white)', borderTop:'4px solid var(--gray)', overflow: 'auto'}}>
-     <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)',  '@media (max-width: 960px)': {
-          gridTemplateColumns: '1fr',
-        }, }}>
-
+    <div
+      style={{
+        maxWidth: "100%",
+        background: "var( --black)",
+        color: "var(--white)",
+        borderTop: "4px solid var(--gray)",
+        overflow: "auto",
+      }}
+    >
+      <Box
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "repeat(4, 1fr)",
+          "@media (max-width: 960px)": {
+            gridTemplateColumns: "1fr",
+          },
+        }}
+      >
         <Item>
-          <NavLink to='/about'><StyledTypography >About Us </StyledTypography></NavLink>
-         <StyledDivider></StyledDivider> 
-          <StyledTypography sx={{fontWeight:'500'}}>
-             We are a family-owned business dedicated to producing high-quality honey and honey-related products. 
-             We organize educational events at our cozy farm where you can learn about hardworking creatures, their importance, 
-             and how you can play your part in saving them.
-           </StyledTypography>
-        </Item>
-      
-        <Item>
-          <NavLink to='/home'><StyledTypography>Honey&Products</StyledTypography></NavLink>
-          <StyledDivider></StyledDivider> 
-
-          <List >
-              <ListItem sx={{ paddingLeft: 0 }}>
-                <ListItemIcon><ArrowForwardIosIcon sx={{color:'var(--yellow)'}}/></ListItemIcon>
-                 <NavLink to='/home'><ListItemText  primary="Home"  primaryTypographyProps={listItemTextStyle}/></NavLink>
-              </ListItem>
-
-              <ListItem sx={{ paddingLeft: 0 }}>
-                <ListItemIcon><ArrowForwardIosIcon sx={{color:'var(--yellow)'}}/></ListItemIcon>
-                 <NavLink to='/about'><ListItemText  primary="About" primaryTypographyProps={listItemTextStyle}/></NavLink>
-              </ListItem>
-
-             <ListItem sx={{ paddingLeft: 0 }}>
-               <ListItemIcon><ArrowForwardIosIcon sx={{color:'var(--yellow)'}}/></ListItemIcon>
-                 <NavLink to='/shop'><ListItemText primary="Shop" primaryTypographyProps={listItemTextStyle}/></NavLink>
-             </ListItem>
-
-             <ListItem sx={{ paddingLeft: 0 }}>
-               <ListItemIcon><ArrowForwardIosIcon sx={{color:'var(--yellow)'}}/></ListItemIcon>
-                <NavLink to='contact'> <ListItemText primary="Contact" primaryTypographyProps={listItemTextStyle}/></NavLink>
-             </ListItem>
-           </List>
+          <NavLink to="/about">
+            <StyledTypography>About Us </StyledTypography>
+          </NavLink>
+          <StyledDivider></StyledDivider>
+          <StyledTypography sx={{ fontWeight: "500" }}>
+            We are a family-owned business dedicated to producing high-quality
+            honey and honey-related products. We organize educational events at
+            our cozy farm where you can learn about hardworking creatures, their
+            importance, and how you can play your part in saving them.
+          </StyledTypography>
         </Item>
 
-     <Item>
+        <Item>
+          <NavLink to="/home">
+            <StyledTypography>Honey&Products</StyledTypography>
+          </NavLink>
+          <StyledDivider></StyledDivider>
+
+          <List>
+            <ListItem sx={{ paddingLeft: 0 }}>
+              <ListItemIcon>
+                <ArrowForwardIosIcon sx={{ color: "var(--yellow)" }} />
+              </ListItemIcon>
+              <NavLink to="/home">
+                <ListItemText
+                  primary="Home"
+                  primaryTypographyProps={listItemTextStyle}
+                />
+              </NavLink>
+            </ListItem>
+
+            <ListItem sx={{ paddingLeft: 0 }}>
+              <ListItemIcon>
+                <ArrowForwardIosIcon sx={{ color: "var(--yellow)" }} />
+              </ListItemIcon>
+              <NavLink to="/about">
+                <ListItemText
+                  primary="About"
+                  primaryTypographyProps={listItemTextStyle}
+                />
+              </NavLink>
+            </ListItem>
+
+            <ListItem sx={{ paddingLeft: 0 }}>
+              <ListItemIcon>
+                <ArrowForwardIosIcon sx={{ color: "var(--yellow)" }} />
+              </ListItemIcon>
+              <NavLink to="/shop">
+                <ListItemText
+                  primary="Shop"
+                  primaryTypographyProps={listItemTextStyle}
+                />
+              </NavLink>
+            </ListItem>
+
+            <ListItem sx={{ paddingLeft: 0 }}>
+              <ListItemIcon>
+                <ArrowForwardIosIcon sx={{ color: "var(--yellow)" }} />
+              </ListItemIcon>
+              <NavLink to="contact">
+                {" "}
+                <ListItemText
+                  primary="Contact"
+                  primaryTypographyProps={listItemTextStyle}
+                />
+              </NavLink>
+            </ListItem>
+          </List>
+        </Item>
+
+        <Item>
           <StyledTypography>Get In Touch </StyledTypography>
-        <StyledDivider></StyledDivider> 
+          <StyledDivider></StyledDivider>
 
-         {/* Contact icons and info */}
-           <List>
-        {contactInfo.map((item, index) => (
-         <ListItem key={index} sx={{ paddingLeft: 0}}>
-           <ListItemIcon sx={{color: 'var(--yellow)'}}>{item.icon}</ListItemIcon>
-           <ListItemText primary={item.text} primaryTypographyProps={listItemTextStyle}/>
-         </ListItem>
-             ))}
-        </List>
-     </Item>
+          {/* Contact icons and info */}
+          <List>
+            {contactInfo.map((item, index) => (
+              <ListItem key={index} sx={{ paddingLeft: 0 }}>
+                <ListItemIcon sx={{ color: "var(--yellow)" }}>
+                  {item.icon}
+                </ListItemIcon>
+                <ListItemText
+                  primary={item.text}
+                  primaryTypographyProps={listItemTextStyle}
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Item>
 
-     <Item>      
-        <div>
-          {/* social icons */}
-         <List sx={{ display: 'flex', flexDirection: 'row', color: 'var(--yellow)' }}>
-         {icons.map((icon, index) => (
-         <ListItem key={index} sx={{cursor:'pointer'}}>{icon}</ListItem> 
-         ))}
-         </List>
-       </div>
+        <Item>
+          <div>
+            {/* social icons */}
+            <List
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                color: "var(--yellow)",
+              }}
+            >
+              {icons.map((icon, index) => (
+                <ListItem key={index} sx={{ cursor: "pointer" }}>
+                  {icon}
+                </ListItem>
+              ))}
+            </List>
+          </div>
 
-      <Grid container spacing={2} sx={{paddingTop:'1.5rem'}}>
-        <Grid item xs={12} sm={6} >
-          <CustomTextField
-            required
-            fullWidth
-            label="Your Name"
-            name="name"
-            autoComplete="name"
-            InputLabelProps={{ shrink: false }}
-          />
-        </Grid>
+          <Grid container spacing={2} sx={{ paddingTop: "1.5rem" }}>
+            <Grid item xs={12} sm={6}>
+              <CustomTextField
+                required
+                fullWidth
+                label="Your Name"
+                name="name"
+                autoComplete="name"
+                InputLabelProps={{ shrink: false }}
+              />
+            </Grid>
 
-        <Grid item xs={12} sm={6}>
-          <CustomTextField
-            required
-            fullWidth
-            type="email"
-            label="E-mail"
-            name="email"
-            autoComplete="email"
-            InputLabelProps={{ shrink: false }}
-          />
-        </Grid>
+            <Grid item xs={12} sm={6}>
+              <CustomTextField
+                required
+                fullWidth
+                type="email"
+                label="E-mail"
+                name="email"
+                autoComplete="email"
+                InputLabelProps={{ shrink: false }}
+              />
+            </Grid>
 
-        <Grid item xs={12} sm={12}>
-          <CustomTextField
-            required
-            fullWidth
-            multiline
-            rows={4}
-            label="Message..."
-            name="message"
-            InputLabelProps={{ shrink: false }}
-          />
-        </Grid>
+            <Grid item xs={12} sm={12}>
+              <CustomTextField
+                required
+                fullWidth
+                multiline
+                rows={4}
+                label="Message..."
+                name="message"
+                InputLabelProps={{ shrink: false }}
+              />
+            </Grid>
 
-        <Grid item xs={12}>
-          <StyledButton type="submit" variant="contained">
-            Submit
-          </StyledButton>
-        </Grid>
-
-      </Grid>
-    </Item>
+            <Grid item xs={12}>
+              <StyledButton type="submit" variant="contained">
+                Submit
+              </StyledButton>
+            </Grid>
+          </Grid>
+        </Item>
       </Box>
 
-        <div className='copyright' style={{display:'flex', justifyContent:'center'}}>
-          <StyledTypography>&copy; Honey&Products  {new Date().getFullYear()}</StyledTypography>
-        </div>
-     
-  </div>
-
+      <div
+        className="copyright"
+        style={{ display: "flex", justifyContent: "center" }}
+      >
+        <StyledTypography>
+          &copy; Honey&Products {new Date().getFullYear()}
+        </StyledTypography>
+      </div>
+    </div>
   );
 }
-
-
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,8 +1,9 @@
-import * as React from 'react';
-import productData from '../Shop/productData.json'; 
-import Rating from '@mui/material/Rating';
+import * as React from "react";
+import productData from "../Shop/productData.json";
+import Rating from "@mui/material/Rating";
+import "./testimonials.css";
 
-
+/* Shuffles reviews, new reviwes appear every time the page refreshed */
 function shuffleArray(array) {
   const shuffledArray = [...array];
   for (let i = shuffledArray.length - 1; i > 0; i--) {
@@ -15,43 +16,26 @@ function shuffleArray(array) {
 // Shuffle the productData array and take the first three random items
 const shuffledProductData = shuffleArray(productData);
 
-export default function Testimonials(){
-    return(
-        <>
-       <h1 style={{textAlign:'center', padding:'1rem 0', fontFamily:'var(--main-font)', fontWeight:'bolder'}}>
-        What our customers say</h1>
-        <div className='card-wrapper' style={{display:'flex', flexDirection:'row',  justifyContent:'space-around', alignItems:'center', flexWrap: 'wrap' }}>
-{shuffledProductData.slice(0, 3 ).map((cardItem) => (
-<div key={cardItem.id} className='card-container'  
-style={{ padding: '1.5em',
-        flex: 1,
-        maxWidth: '400px',
-        margin: '1em'}}>
-
-{/* it causes a bug, remove and take first 3 reviews or... */}
-
-
-
-<div  className='card-content'  style={{display:'flex', flexDirection:'column', width: '100%',
-          height: '500px',
-          padding: '1.5em',
-          borderRadius: '15px',
-          background: 'var(--white)',
-          boxShadow: '-22px -22px 0px 0px var(--black)', }} >
-   <img src={cardItem.reviewerImg} style={{borderRadius:'50%', border:'2px solid var(--dark-yellow)', width: '150px', height:'150px'}}/>
-<h2>{cardItem.reviewer}</h2>
- <Rating name="read-only" value={cardItem.value} readOnly />
-<h3>{cardItem.title}</h3>
-<p>{cardItem.review}</p>
-<h4>{cardItem.dateReview}</h4>
-</div>
-</div>
-))}
-</div>
-
-
-        </>
-    )
+export default function Testimonials() {
+  return (
+    <>
+      <h1>What our customers say</h1>
+      <div className="card-wrapper">
+        {shuffledProductData.slice(0, 3).map((cardItem) => (
+          <div key={cardItem.id} className="card-container">
+            <div className="card-content">
+              <div className="card-img-container">
+                <img src={cardItem.reviewerImg} />
+              </div>
+              <h2>{cardItem.reviewer}</h2>
+              <Rating name="read-only" value={cardItem.value} readOnly />
+              <h3>{cardItem.title}</h3>
+              <p>{cardItem.review}</p>
+              <h4>{cardItem.dateReview}</h4>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
-
-//add more photos of reviewers 
