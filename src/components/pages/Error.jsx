5da03d072f57a936fda9ffc08df4f61f77af3b69
KarynaMisnik <karinamisnik94@gmail.com>
2023-11-15 21:27:01 +0200

    error page updated
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

--- src/components/pages/Error.jsx
+++ src/components/pages/Error.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from "react";
 
-
-export default function Error(){
+export default function Error() {
   return (
-   <div className='error-container' style={{display:'flex',justifyContent:'center',alignItems:'center', background: "var(--light-yellow)", maxWidth: "100%", height: "auto", position: 'relative'}}>
-   <h1>Here must be smth</h1>
-    <div className='text-container'>
-<p><span>404</span><br/> Oh, bzzz. <br></br>You are totally lost.</p>
-    </div>
+    <div className="error-container">
+      <div className="text-container">
+        <span>404</span>
+        <p>Oh, bzzz.</p>
+        <p> You are totally lost.</p>
+      </div>
+      <div className="error-img-container">
+        <h1>Here must be smth</h1>
+      </div>
     </div>
   );
-};
-
+}
