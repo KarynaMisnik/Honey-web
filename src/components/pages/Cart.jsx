992e1d48279ebe6cbcd951718456daaadf874a96
KarynaMisnik <karinamisnik94@gmail.com>
2023-11-21 20:12:54 +0200

    Cart updated
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

--- src/components/pages/Cart.jsx
+++ src/components/pages/Cart.jsx
@@ -1,4 +1,18 @@
 import React, { useState, useEffect } from "react";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Divider from "@mui/material/Divider";
+import { styled } from "@mui/material";
+
+const CustomDivider = styled(Divider)(({ theme }) => ({
+  width: "50%",
+  borderBottomWidth: "2px",
+  background: "var(--black)",
+}));
+
+const CustomButton = styled(Button)(({ theme }) => ({
+  padding: ".5rem",
+}));
 
 const Cart = ({ cart, setCart, handleChange }) => {
   const [price, setPrice] = useState(0);
@@ -19,43 +33,45 @@ const Cart = ({ cart, setCart, handleChange }) => {
   });
 
   return (
-    <div>
+    <div className="cart-wrapper">
       {cart?.map((item) => (
-        <div key={item.id}>
-          <div className="img">
+        <div className="product-cntainer" key={item.id}>
+          <div className="cart-item-img">
+            <h4>{item.title}</h4>
             <img src={item.src} alt={item.alt} />
-            <p>{item.title}</p>
           </div>
+          <div className="all-btn-wrapper">
+            <div>
+              <ButtonGroup
+                variant="contained"
+                aria-label="outlined primary button group"
+              >
+                <CustomButton onClick={() => handleChange(item, +1)}>
+                  +
+                </CustomButton>
+                <CustomButton>{item.count}</CustomButton>
+                <CustomButton onClick={() => handleChange(item, -1)}>
+                  -
+                </CustomButton>
+              </ButtonGroup>
+            </div>
 
-          <div className="btn-container">
-            <button
-              style={{ padding: ".5rem", background: "#c2f98e" }}
-              onClick={() => handleChange(item, +1)}
-            >
-              +
-            </button>
-            <button>{item.count}</button>
-            <button
-              style={{ padding: ".5rem", background: "#fbbfb5" }}
-              onClick={() => handleChange(item, -1)}
-            >
-              -
-            </button>
-          </div>
-          <div>
-            <span>{item.price}</span>
-            <button
-              style={{ color: "white", background: "red", padding: ".5rem" }}
-              onClick={() => handleRemove(item.id)}
-            >
-              Remove
-            </button>
+            <div className="price-and-remove-btn">
+              <span>{item.price} &#8364;</span>
+              <button
+                style={{ color: "white", background: "red", padding: ".5rem" }}
+                onClick={() => handleRemove(item.id)}
+              >
+                Remove
+              </button>
+            </div>
           </div>
         </div>
       ))}
-      <div>
+      <CustomDivider />
+      <div className="total-sum">
         <span>Total: </span>
-        <span>&#8364; - {price}</span>
+        <span> {price} &#8364;</span>
       </div>
     </div>
   );
