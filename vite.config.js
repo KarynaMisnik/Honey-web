531a2ade7aa8a5925ebccafeef92de6f22755772
KarynaMisnik <karinamisnik94@gmail.com>
2024-01-18 14:10:39 +0200

    .
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://karynamisnik.github.io/bee_e-spa",
+  base: "/bee_e-spa/",
 });
