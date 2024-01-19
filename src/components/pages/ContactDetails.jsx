52c27b1994c7cd89f6a164e880448e7bc4ed489c
KarynaMisnik <karinamisnik94@gmail.com>
2024-01-19 15:44:30 +0200

    deployment
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

--- src/components/pages/ContactDetails.jsx
+++ src/components/pages/ContactDetails.jsx
@@ -17,21 +17,21 @@ export default function ContactDetails() {
       title: "Work Together",
       content:
         "Are you interested in collaborating with our family Honey Business? Whether you are a local beekeeper, a distributor looking for unique honey products, or an artisan interested in partnering with us, we would love to explore opportunities to work together. Feel free to contact us with your ideas and proposals.",
-      imageUrl: "src/assets/img/contactImgs/workTogether.jpg",
+      imageUrl: "/workTogether.jpg",
     },
     {
       id: 2,
       title: "Customer Care",
       content:
         "At Honey&Products, our customers are at the heart of everything we do. We value your feedback and strive to provide the best possible experience. If you have any suggestions, concerns, or compliments, please do not hesitate to share them with us.",
-      imageUrl: "src/assets/img/contactImgs/customCare.jpg",
+      imageUrl: "/img/contactImgs/customCare.jpg",
     },
     {
       id: 3,
       title: "Visit Us",
       content:
         "We extend a warm invitation to everyone, including schoolchildren and small groups of visitors, to come and experience the magic of our honey farm in Lehtola. If you are planning an educational field trip or looking for a unique experience for your group, book your visit in advance, and let us create unforgettable memories together.",
-      imageUrl: "src/assets/img/contactImgs/visitUs.jpg",
+      imageUrl: "/img/contactImgs/visitUs.jpg",
     },
   ];
 
