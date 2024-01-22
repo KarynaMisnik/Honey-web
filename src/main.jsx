935ec77335175b2f74807111edf466af9ca3de63
KarynaMisnik <karinamisnik94@gmail.com>
2024-01-22 13:05:13 +0200

    trying to use hashRouter to fix 404 error
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router basename="/Honey-web">
+  <Router>
     <App />
   </Router>
 );
+
+{
+  /* basename="/Honey-web" */
+}
