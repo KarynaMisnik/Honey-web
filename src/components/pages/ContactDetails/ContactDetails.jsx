6b4034c420c1806a90a9d1657b9c73db270bdeec
KarynaMisnik <karinamisnik94@gmail.com>
2024-01-22 11:11:52 +0200

    add repo name in img path in shop and about pages
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

--- src/components/pages/ContactDetails/ContactDetails.jsx
+++ src/components/pages/ContactDetails/ContactDetails.jsx
@@ -24,14 +24,14 @@ export default function ContactDetails() {
       title: "Customer Care",
       content:
         "At Honey&Products, our customers are at the heart of everything we do. We value your feedback and strive to provide the best possible experience. If you have any suggestions, concerns, or compliments, please do not hesitate to share them with us.",
-      imageUrl: "/img/contactImgs/customCare.jpg",
+      imageUrl: "/Honey-web/img/contactImgs/customCare.jpg",
     },
     {
       id: 3,
       title: "Visit Us",
       content:
         "We extend a warm invitation to everyone, including schoolchildren and small groups of visitors, to come and experience the magic of our honey farm in Lehtola. If you are planning an educational field trip or looking for a unique experience for your group, book your visit in advance, and let us create unforgettable memories together.",
-      imageUrl: "/img/contactImgs/visitUs.jpg",
+      imageUrl: "/Honey-web/img/contactImgs/visitUs.jpg",
     },
   ];
 
